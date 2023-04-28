30e0cce54b7444d9ff0e9423537abea72cc7d929
Julian0510 <juldiaz@csumb.edu>
2023-04-27 23:47:59 -0700

    zipfile for testing and updated similarity checker code. Can now automatically compare files within a zip archive

--- similarityChecker.py
+++ similarityChecker.py
@@ -3,6 +3,7 @@ import zipfile
 import re
 import pathlib
 
+
 # Java keywords list
 keywords_java = ["public", "void", "private", "boolean", "true",
                  "false", "null", "abstract", "assert", "int", "double", "else",
@@ -54,13 +55,6 @@ def openFile(filename):
 
 def similarityChecker(contents1, contents2, keywords):
 
-    # # reading the file
-    # with open(filename1) as fp:
-    #     contents1 = fp.read()
-
-    # with open(filename2) as fp:
-    #     contents2 = fp.read()
-
     # Remove comments - first in-line comments, next block comments, lastly JavaDoc comments
     contents1 = re.sub("//.*", "", contents1)
     contents1 = re.sub("/\\*.*?\\*/", "", contents1)
@@ -83,14 +77,12 @@ def similarityChecker(contents1, contents2, keywords):
             contents2 = contents2.replace(contentsArray2[i], "")
 
     # Remove whitespace and tabs
-    # contents1 = contents1.replace("\\s", "")
-    # contents2 = contents2.replace("\\s", "")
     contents1 = re.sub("\\s", "", contents1)
     contents2 = re.sub("\\s", "", contents2)
 
+    # # code for testing purposes
     # print(name1, ":", contents1)
     # print(name2, ":", contents2)
-    # # code for testing purposes
     # with open("newfile.java", "w") as fp:
     #     fp.writelines(contents1)
     # with open("newfile2.java", "w") as fp:
@@ -99,60 +91,104 @@ def similarityChecker(contents1, contents2, keywords):
     # Compute the Levenshtein distance
     similarityScore = abs((1 - levenshtein_distance(contents1,
                           contents2) / min(len(contents1), len(contents2)))) * 100
-    print("Similarity score is: " + str(similarityScore) + "%")
+    similarityScore = float('%.2f' % (similarityScore))
+    #print("Similarity score is: " + str(similarityScore) + "%")
 
-    # Prints out the result of comparing two files
-    if similarityScore == 100:
-        print("The two files are identical!")
-    elif similarityScore > 70:
-        print("There are a lot of similarities between the two files.")
-    elif similarityScore > 30:
-        print("There are some resemblances between the two files.")
-    else:
-        print("No worries! There appears to be no sign of plagiarism.")
+    # # Prints out the result of comparing two files
+    # if similarityScore == 100:
+    #     print("The two files are identical!")
+    # elif similarityScore > 70:
+    #     print("There are a lot of similarities between the two files.")
+    # elif similarityScore > 30:
+    #     print("There are some resemblances between the two files.")
+    # else:
+    #     print("No worries! There appears to be no sign of plagiarism.")
+
+    return similarityScore
 
 
 # Driver code for testing the function
-# the zip filenames of the two homework files that we will compare
-print("Enter the name of the first file you will be comparing: ")
-filename1 = input()
-contents1 = openFile(filename1)
-file_extension1 = fileExtensionFinder(filename1)
 
-print("Enter the name of the second file you will be comparing: ")
-filename2 = input()
-contents2 = openFile(filename2)
-file_extension2 = fileExtensionFinder(filename2)
+# the name of the zipfile that has all of the homework solutions we will be comparing
+zipfile1 = "ElevenToothpicksSubmissions.zip"
 
-# for reading files in zip format. Unused as of now
 
-# zipfile1 = "ElevenToothpicksSubmissions/P1.zip"
-# zipfile2 = "ElevenToothpicksSubmissions/W1.zip"
+# reading the contents of zipfile1
+with zipfile.ZipFile(zipfile1, "r") as thezip:
+    files = thezip.namelist()
 
-# # reading the contents of zipfile1
-# with zipfile.ZipFile(zipfile1, "r") as thezip:
-#     files = thezip.namelist()  # should only be 1 file here!
-#     file_extension1 = fileExtensionFinder(files[0])
-#     with thezip.open(files[0]) as fp:
-#         contents1 = fp.read()
-#         contents1 = contents1.decode()  # decode the contents to be readable as a str
+    all_info = []
+    for file in files:
+        temp_info = []
+        # get the file name without the entire path variable
+        filename = file.split("/")[-1]
+        temp_info.append(filename)
 
-# # reading the contents of zipfile2
-# with zipfile.ZipFile(zipfile2, "r") as thezip:
-#     files = thezip.namelist()  # should only be 1 file here!
-#     file_extension2 = fileExtensionFinder(files[0])
-#     with thezip.open(files[0]) as fp:
-#         contents2 = fp.read()
-#         contents2 = contents2.decode()  # decode the contents to be readable as a str
+        with zipfile.ZipFile(file, "r") as z:
+            contents_list = z.namelist()  # should only be one!
+            file_extension = fileExtensionFinder(contents_list[0])
 
-if file_extension1 == file_extension2:
-    if file_extension1 == ".java":
-        similarityChecker(contents1, contents2, keywords_java)
-    elif file_extension1 == ".cpp":
-        print("C++")
-        similarityChecker(contents1, contents2, keywords_cpp)
-    else:
-        print("Unsupported file type! Only C++ or Java file types supported!")
-else:
-    raise ValueError(
-        "file types are not the same. Please make sure that both files are of the same type")
+            with z.open(contents_list[0]) as fp:
+                contents = fp.read()
+                contents = contents.decode()  # decode the contents to be readable as a str
+
+                temp_info.append(contents)
+                temp_info.append(file_extension)
+
+        all_info.append(temp_info)
+
+# # for testing
+# for i in range(0, len(all_info)):
+#     print(all_info[i][0])  # filenames should be printed
+
+
+similarityScores = []
+
+for i in range(0, len(all_info)):
+    target_file = all_info[i]
+    file_extension1 = all_info[i][2]
+    contents1 = all_info[i][1]
+    simiScores_main = []
+    simiScores_main.append(all_info[i][0])
+    for k in range(0, len(all_info)):
+        # check to make sure we don't compare the same file with itself
+        if all_info[k][0] == all_info[i][0]:
+            continue
+        else:
+            file_extension2 = all_info[k][2]
+
+            if file_extension1 == file_extension2:
+                simiScores_temp = []
+                simiScores_temp.append(all_info[k][0])
+
+                if file_extension1 == ".java":
+                    contents2 = all_info[k][1]
+                    similarityScore = similarityChecker(
+                        contents1, contents2, keywords_java)
+                    simiScores_temp.append(similarityScore)
+                elif file_extension1 == ".cpp":
+                    contents2 = all_info[k][1]
+                    similarityScore = similarityChecker(
+                        contents1, contents2, keywords_cpp)
+                    simiScores_temp.append(similarityScore)
+                else:
+                    print(
+                        "Unsupported file type! Only C++ or Java file types supported!")
+            else:
+                raise ValueError(
+                    "file types are not the same. Please make sure that both files are of the same type")
+
+            simiScores_main.append(simiScores_temp)
+
+    similarityScores.append(simiScores_main)
+
+
+for i in range(0, len(similarityScores)):
+    print("Here is a list of files that were flagged as similar to",
+          similarityScores[i][0], ":")
+    for k in range(0, len(similarityScores[i])):
+        if float(similarityScores[i][k][1]) >= 70.0:
+            print("\t", similarityScores[i][k][0],
+                  "had a similarity score of:", similarityScores[i][k][1], "%")
+
+    print("")
